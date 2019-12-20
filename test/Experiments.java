b9642e805f5d28771af681abb3211de62eb16f65
Royar <royar13@gmail.com>
2019-12-20 12:48:11 +0200

    Start working on experiments

---
+++ test/Experiments.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Experiments {
+	@Test
+	void exp1() {
+		for (int i = 1; i <= 10; i++) {
+			AVLTree tree = new AVLTree();
+			int n = i * 10000;
+			List<Integer> keys = new ArrayList<>();
+			System.out.println("i: " + 1);
+			for (int j = 0; j < n; j++) {
+
+			}
+			Collections.sort(keys);
+		}
+	}
+}
